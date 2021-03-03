c97e9d0115c5f78b4ace093350864e7e35459fd6
Alastair Shone <alastairshone@Alastairs-MacBook-Pro-2.local>
2021-03-03 16:23:00 +0000

    updated packagejson, postcss config and added nesting for css

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,28 @@ const path = require( "path" );
 module.exports = {
 	mode: "development",
 	entry: "./src/js/app.js",
+	module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  },
 	output: {
 		path: path.resolve(__dirname, "public"),
 		filename: "app.bundle.js"
