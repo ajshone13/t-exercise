426e09866396a96987be65f61538d392ebb93c2e
Alastair Shone <alastairshone@Alastairs-MacBook-Pro-2.local>
2021-03-03 16:31:30 +0000

    added redirect to pass ac, adding plan for article, passing data to article

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const router = require('./router');
+
+// DATA
 const meta = require("./content/meta.json");
+const article = require("./content/article.json");
+const related = require("./content/posts.json");
+
 const app = express();
 const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
 app.set('meta', meta);
+app.set('article', article.post);
+app.set('related', related.posts);
 
 app.use('*/static', express.static('public'));
 
--- server/router.js
+++ server/router.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// redirect to specific url
+router.get('/', (req , res) => {
+	res.redirect('/einstein-and-churchill-both-took-daily-naps');
+});
+
+router.get('/einstein-and-churchill-both-took-daily-naps', (req, res) => {
 	res.render("home", {
-		meta: req.app.get('meta')
+		meta: req.app.get('meta'),
+		article: req.app.get('article'),
+		related: req.app.get('related')
 	});
 });
 
