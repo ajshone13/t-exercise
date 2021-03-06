301f9a733a7221043e8a26339e62702308df4c91
Alastair Shone <alastairshone@Alastairs-MacBook-Pro-2.local>
2021-03-06 21:00:51 +0000

    updated tests

--- src/js/comments.spec.js
+++ src/js/comments.spec.js
@@ -18,18 +18,23 @@ let mockCommentsData = [
   }
 ];
 
+const mockDocument = '<ul class="article__comments-list"><div class="article__comments-num"></div><div class="article__comments-sort">Sort: <a class="sort__likes">Likes</a> | <a class="sort__date">Newest</a></div></ul>';
+
+global.fetch = () =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockCommentsData)
+  });
+
 describe('Comments', () => {
   beforeEach(() => {
-    global.fetch = () =>
-      Promise.resolve({
-        json: () => Promise.resolve(mockCommentsData),
-      });
-
       comments = new Comments();
-      document.body.innerHTML = '<ul class="article__comments-list"><div class="article__comments-num"></div><div class="article__comments-sort">Sort: <a class="sort__likes">Likes</a> | <a class="sort__date">Newest</a></div></ul>';
+      document.body.innerHTML = mockDocument;
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = mockDocument;
   });
   
-
   test("comments exists", () => {
     expect(Comments).toBeDefined();
   });
@@ -44,21 +49,28 @@ describe('Comments', () => {
     const getCommentsData = jest.spyOn(comments, 'getCommentsData');
     await comments.getCommentsData();
     expect(getCommentsData).toHaveBeenCalled();
+    expect(document.querySelector('.article__comments-num span').innerHTML).toBe('2 comments');
   });
 
-  test("commentHTMLMarkup", () => {
+  test("commentHTMLMarkup", async () => {
+    await comments.getCommentsData();
+
     const commentHTMLMarkup = jest.spyOn(comments, 'commentHTMLMarkup');
     comments.commentHTMLMarkup(mockCommentsData);
     expect(commentHTMLMarkup).toHaveBeenCalled();
   });
 
-  test("appendComments", () => {
+  test("appendComments", async () => {
+    await comments.getCommentsData();
+
     const appendComments = jest.spyOn(comments, 'appendComments');
     comments.appendComments(mockCommentsData);
     expect(appendComments).toHaveBeenCalled();
   });
 
-  test("sortBy", () => {
+  test("sortBy", async () => {
+    await comments.getCommentsData();
+
     const sortBy = jest.spyOn(comments, 'sortBy');
     const appendComments = jest.spyOn(comments, 'appendComments');
     comments.sortBy(mockCommentsData);
@@ -69,12 +81,32 @@ describe('Comments', () => {
     expect(appendComments).toHaveBeenCalled();
   });
 
-  test("clearComments", () => {
+  test("clearComments", async () => {
+    await comments.getCommentsData();
+
     const clearComments = jest.spyOn(comments, 'clearComments');
-    console.log(document.querySelector('.article__comments-num').children);
+    expect(document.querySelector('.article__comments-num span').innerHTML).toBe('2 comments');
     comments.clearComments();
+    console.log(document.querySelector('.article__comments-num span').innerHTML);
     expect(clearComments).toHaveBeenCalled();
-    expect(document.querySelector('.article__comments-num').innerHTML).toBe('');
+  });
+
+  test("returns undefined when exception", async () => {
+    fetch = () =>
+      Promise.resolve({
+        json: () => Promise.reject("ERR"),
+      });
+    const data = await comments.getCommentsData();
+    expect(data).toEqual(undefined);
+  });
+
+  test("returns undefined when no data", async () => {
+    fetch = () =>
+      Promise.resolve({
+        json: () => Promise.resolve('')
+      });
+    const data = await comments.getCommentsData();
+    expect(data).toEqual(undefined);
   });
 
 });
